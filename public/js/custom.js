23f18fcbc0d029784cb4b1bbb2cdcfe0821e3148
Lucho Martin <luchoid@gmail.com>
2025-09-09 12:58:00 +0200

    update

--- public/js/custom.js
+++ public/js/custom.js
@@ -39,7 +39,16 @@
   $(document).scroll(function(){
     onHabilidades();
 		$(window).resize();
+
+		const scrollIndicator = document.getElementById("scroll-indicator");
+	  const maxScrollableHeight = document.body.scrollHeight - window.innerHeight;
+	  const percentage = (window.scrollY / maxScrollableHeight) * 100;
+	  scrollIndicator.style.width = `${percentage}%`;
   });
+
+	/*window.addEventListener("scroll", () => {
+
+	});*/
 
 
 	/*let magneticArea = document.querySelector('.logo.l-1');
